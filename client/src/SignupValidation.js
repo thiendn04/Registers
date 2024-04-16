f70830f79b2143653a7c81ab2aa333cb50757e37
Thiendn04 <thiendn04@gmail.com>
2024-04-16 11:09:38 +0700

    sss

--- client/src/SignupValidation.js
+++ client/src/SignupValidation.js
@@ -2,8 +2,7 @@ function Validation(values) {
     let error = {}
     const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     //const password_pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
-    const password_pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,}$/
-
+    const password_pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?\\/~`-])[A-Za-z\d!@#$%^&*()_+{}\[\]:;<>,.?\\/~`-]{8,}$/;
 
     if(values.name === "") {
         error.name = "Name should not be empty"
